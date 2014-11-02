78893e426085ac7ab75c2b2932594a9d78621bd6
Christoffer Winterkvist <chris@zenangst.com>
2014-11-02 12:01:00 +0100

    Initial Commit

---
+++ wd/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  wd
+//
+//  Created by Christoffer Winterkvist on 11/2/14.
+//  Copyright (c) 2014 zenangst. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
